c02d33efffa80508aebbc02c6b9d819621f9c9f8
markus <ms@dom.de>
2025-09-24 11:20:46 +0200

    videoconvert added

--- rtp_server.py
+++ rtp_server.py
@@ -72,7 +72,8 @@ def build_pipeline(args: Any) -> Tuple[Gst.Pipeline, str]:
   v4l2src device={LEFT} io-mode=4
   ! videoconvert
   ! perspective
-  ! video/x-raw,format=NV12,width=1920,height=1080,framerate={FRAMES}/1 
+  ! video/x-raw,format=NV12,width=1920,height=1080,framerate={FRAMES}/1
+  ! videoconvert 
   ! videoflip method=counterclockwise 
   ! queue max-size-buffers=2 max-size-time=33333333 leaky=2 
   ! stitch.sink_0 
