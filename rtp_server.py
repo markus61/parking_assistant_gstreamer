a3e4198f82f36baac15e7df37b465b662ba83c1c
markus <ms@dom.de>
2025-10-06 19:20:23 +0200

    pipeline

--- rtp_server.py
+++ rtp_server.py
@@ -39,9 +39,9 @@ def build_pipeline(args: Any) -> str:
 v4l2src device=/dev/video31 io-mode=4
     ! video/x-raw,format=NV12,width=1280,height=720,framerate=10/1
     ! glupload ! glcolorconvert
+    ! gltransformation ortho=false fov=59 rotation-x=0 rotation-y=0 rotation-z=90 pivot-x=0
 """
     better_pipeline = f"""
-    ! gltransformation ortho=false fov=59 rotation-x=0 rotation-y=0 rotation-z=90 pivot-x=0
     ! glcolorscale
     ! 'video/x-raw,format=(string)RGBA,width=340,height=640,framerate=15/1'
     ! gldownload ! fakevideosink
