0dd1dc6fae32230d925e546f9c9bcee15b5ed19b
markus <ms@dom.de>
2025-10-09 18:22:12 +0200

    pretty nice

--- pylib/gstreamer.py
+++ pylib/gstreamer.py
@@ -16,13 +16,13 @@ Gst.init(sys.argv[1:])
 
 class GstElement():
     def __init__(self, element: str, name: str):
-        name = "" if not name else name
         self.element = Gst.ElementFactory.make(element, name)
-        self._name = name
+        self._name = self.element.get_name() if self.element else ""
+        self.pipeline = None
 
     @property
     def name(self) -> str:
-        return self.element.get_name()
+        return self._name
     @property
     def src(self) -> Gst.Element:
         return self.element.get_static_pad("src")
@@ -36,65 +36,45 @@ class GstElement():
 
 
 class Pipeline():
-    def __init__(self, name: str = "", element: GstElement = None):
-        name = "tube" if not name else name
-        self.pipeline = Gst.Pipeline.new(name) or None
-        self.tail = element or None
+    def __init__(self, element: GstElement = None, name: str = None ):
+        self.tail = None
+        if element and element.pipeline:
+            self.pipeline = element.pipeline
+        else:
+            self.pipeline = Gst.Pipeline.new(name) or None
         if element:
-            self.pipeline.add(element.element)
-
+            self.tail = element
+            if self.pipeline.get_by_name(element.name) is None:
+                self.pipeline.add(element.element)
+ 
     @property
     def gst_pipeline(self) -> Gst.Pipeline:
         return self.pipeline
 
     def append(self, element: GstElement):
         self.pipeline.add(element.element)
+        element.pipeline = self.pipeline
         type_name = element.element.__gtype__.name
 
         if self.tail is None:
             self.tail = element
             return self
 
-        if isinstance(element.sink, list):
-            self.tail.src.link(element.sink.pop())
-        else:
-            self.tail.src.link(element.sink)
+        # Link tail to new element
+        link_result = self.tail.src.link(element.sink)
+
+        # Check if link was successful
+        if link_result != Gst.PadLinkReturn.OK:
+            logger.error(f"Failed to link {self.tail.name} -> {element.name}: {link_result}")
+            raise RuntimeError(f"Pad linking failed: {link_result}")
+
+        logger.debug(f"Linked {self.tail.name} -> {element.name}")
         self.tail = element
         return self
 
-
-class Filter(GstElement):
-    def __init__(self, filter_str: str, name: str = ""):
-        super().__init__("capsfilter", name)
-        caps = Gst.Caps.from_string(filter_str)
-        self.element.set_property("caps", caps)
-
-class GlColorscale(GstElement):
-    def __init__(self, name: str = ""):
-        super().__init__("glcolorscale", name)
-
-class EyePipe(GstElement):
-    def __init__(self, name: str = ""):
-        name = "eye" if not name else name
-        super().__init__("videotestsrc", name)    
-      
-class GlUplPipe(GstElement):
-    def __init__(self, name: str = ""):
-        super().__init__("glupload", name)
-
-class Tee(GstElement):
-    def __init__(self, name = ""):
-        super().__init__("tee", name)
-
-    @property
-    def src(self) -> Gst.Pad:
-        return self.element.get_request_pad("src_%u")
-    def link(self, sink) -> Gst.Element:
-        return self.element.get_request_pad("src_%u").link(sink)
-
 class MxPipe(GstElement):
 
-    def __init__(self, name: str = ""):
+    def __init__(self, name: str = None):
         super().__init__("glvideomixer", name)
 
     @property
@@ -105,59 +85,105 @@ class MxPipe(GstElement):
     def src(self) -> Gst.Pad:
         return self.element.get_static_pad("src")
 
-def create_glmixer(name: str) -> Gst.Element:
-    name = "glmixer" if not name else name
-    glmixer = Gst.ElementFactory.make("glvideomixer", name)
-    left = glmixer.get_request_pad("sink_%u")
-    right = glmixer.get_request_pad("sink_%u")
-    return glmixer, left, right 
+class MyMixClass(MxPipe):
 
-def create_tee(name: str) -> Gst.Element:
-    name = "t" if not name else name
-    t = Gst.ElementFactory.make("tee", name)
-    left = t.get_request_pad("src_%u")
-    right = t.get_request_pad("src_%u")
-    return t, left, right
+    def __init__(self, name: str = None):
+        super().__init__(name)
+        self.inputs = 0
 
-def create_filter(name: str) -> Gst.Element:
-    capsfilter = Gst.ElementFactory.make("capsfilter", name)
-    caps = Gst.Caps.from_string("video/x-raw,format=NV12,width=1280,height=720,framerate=15/1")
-    capsfilter.set_property("caps", caps)
-    return capsfilter
+    @property
+    def sink(self) -> Gst.Element:
+        sink = self.element.get_request_pad("sink_%u")
+        sink.set_property("xpos", self.inputs * 1280)
+        self.inputs = self.inputs + 1
+        return sink
 
-def create_source(name: str) -> Gst.Element:
-    source = Gst.ElementFactory.make("videotestsrc", name)
-    source.set_property("pattern",0)
-    return source
+    @property
+    def src(self) -> Gst.Pad:
+        return self.element.get_static_pad("src")
 
-def create_sink() -> Gst.Element:
-    return Gst.ElementFactory.make("autovideosink", "sink")
+class Filter(GstElement):
+    def __init__(self, filter_str: str, name: str = None):
+        super().__init__("capsfilter", name)
+        caps = Gst.Caps.from_string(filter_str)
+        self.element.set_property("caps", caps)
 
-def create_compositor() -> Gst.Element:
-    comp = Gst.ElementFactory.make("compositor", "comp")
-    left = comp.get_request_pad("sink_%u")
-    right = comp.get_request_pad("sink_%u")
-    right.set_property("xpos", 1280)
-    return comp, left, right
+class GlColorConvert(GstElement):
+    def __init__(self, name: str = None):
+        super().__init__("glcolorconvert", name)
+
+class GlColorscale(GstElement):
+    def __init__(self, name: str = None):
+        super().__init__("glcolorscale", name)
+
+class EyePipe(GstElement):
+    def __init__(self, name: str = None):
+        name = "eye" if not name else name
+        super().__init__("videotestsrc", name)    
+      
+class GlUplPipe(GstElement):
+    def __init__(self, name: str = None):
+        super().__init__("glupload", name)
+
+class Tee(GstElement):
+    def __init__(self, name: str = None):
+        super().__init__("tee", name)
+
+    @property
+    def src(self) -> Gst.Pad:
+        return self.element.get_request_pad("src_%u")
+    def link(self, sink) -> Gst.Element:
+        return self.element.get_request_pad("src_%u").link(sink)
+    def leg(self) -> Pipeline:
+        leg = Pipeline(self)
+        return leg
+
+class XVidSink(GstElement):
+    def __init__(self, name: str = None):
+        super().__init__("ximagesink", name)
+
+class GlVidSink(GstElement):
+    def __init__(self, name: str = None):
+        super().__init__("glimagesinkelement", name)
 
 def create_pipeline() -> Gst.Pipeline:
-    pipeline = Pipeline()
-    left_eye = EyePipe("left_eye")
-    pipeline.append(left_eye)
-    f1 = Filter("video/x-raw,format=NV12,width=1280,height=720,framerate=15/1")
-    pipeline.append(f1)
-    glup = GlUplPipe()
-    pipeline.append(glup)
-    f2=Filter("video/x-raw(memory:GLMemory),format=RGBA,width=1280,height=720,framerate=15/1")
-    pipeline.append(f2)
-    glcolor = GlColorscale()
-    pipeline.append(glcolor)
-    tee = Tee()
-    pipeline.append(tee)
-    mk = MxPipe()
-    pipeline.append(mk)
+    xvidsink = XVidSink()
+    glvidsink = GlVidSink()
+    glcolorconvert = GlColorConvert()
 
-    return pipeline.pipeline
+    original = Pipeline()
+    left_eye = EyePipe("left_eye")
+    original.append(left_eye)
+    f1 = Filter("video/x-raw,format=NV12,width=1280,height=720,framerate=15/1")
+    original.append(f1)
+    #original.append(xvidsink)
+
+    glup = GlUplPipe()
+    f2=Filter("video/x-raw,format=RGBA,width=1280,height=720,framerate=15/1")
+    original.append(glup)
+    original.append(glcolorconvert)
+
+    tee = Tee()
+    original.append(tee)
+    mk = MyMixClass()
+    original.append(mk)
+
+    distorted = tee.leg()
+    distorted.append(mk)
+
+    original.append(glvidsink)
+
+    return original.pipeline
+
+def rest():
+    glcolor = GlColorscale()
+    original.append(glcolor)
+
+    output = Pipeline(mk)
+    xvid = XVidSink()
+    output.append(xvid)
+    print(original.pipeline)
+
 
 def walk_pattern(pipeline: Gst.Pipeline):
     left_eye = pipeline.get_by_name("left_eye")
